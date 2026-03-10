702110576b363f356a4d9798cc7fca50451d0d67
HARISHLAL007 <harishlalme9a@gmail.com>
2026-03-10 14:37:29 +0530

    Harishlal Added UC1 Palindrome Logic

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,2 +1,29 @@
+
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num, reversed = 0, remainder, original;
+
+        System.out.print("Enter a number: ");
+        num = sc.nextInt();
+
+        original = num;
+
+        while (num != 0) {
+            remainder = num % 10;
+            reversed = reversed * 10 + remainder;
+            num = num / 10;
+        }
+
+        if (original == reversed) {
+            System.out.println("Palindrome Number");
+        } else {
+            System.out.println("Not a Palindrome Number");
+        }
+
+        sc.close();
+    }
 }
